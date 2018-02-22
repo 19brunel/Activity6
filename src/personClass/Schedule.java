662aad3ecee737c7c63dacaa5c8bf2b1426197b7
Unknown <19brunel@elmbrookstudents.org>
2018-02-22 16:15:58 -0600

    Finished mySchedule v0.1

--- src/personClass/Schedule.java
+++ src/personClass/Schedule.java
@@ -11,10 +11,20 @@ public class Schedule {
 	public String mySchedule(){
 		String finalString = "";
 		if(scheduledMeetings[0].isEmpty()!=true){
-			finalString = "";
+			finalString = scheduledMeetings[0];
 		}
-		
-		
+		if(scheduledMeetings[1].isEmpty()!=true){
+			finalString = scheduledMeetings[0]+"\n"+scheduledMeetings[1];
+		}
+		if(scheduledMeetings[2].isEmpty()!=true){
+			finalString = scheduledMeetings[0]+"\n"+scheduledMeetings[1]+"\n"+scheduledMeetings[2];
+		}
+		if(scheduledMeetings[3].isEmpty()!=true){
+			finalString = scheduledMeetings[0]+"\n"+scheduledMeetings[1]+"\n"+scheduledMeetings[2]+"\n"+scheduledMeetings[3];
+		}
+		if(scheduledMeetings[4].isEmpty()!=true){
+			finalString = scheduledMeetings[0]+"\n"+scheduledMeetings[1]+"\n"+scheduledMeetings[2]+"\n"+scheduledMeetings[3]+"\n"+scheduledMeetings[4];
+		}
 		return finalString;
 	}
 	
