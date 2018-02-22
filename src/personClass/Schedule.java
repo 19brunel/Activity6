6f18334f3961beaf768aadcf04ad8be63c3327c5
Unknown <19brunel@elmbrookstudents.org>
2018-02-22 16:12:33 -0600

    Added Stuff

--- src/personClass/Schedule.java
+++ src/personClass/Schedule.java
@@ -1,5 +1,22 @@
 package personClass;
 
 public class Schedule {
-
+	
+	private String[] scheduledMeetings;
+	
+	public Schedule(){
+		scheduledMeetings = new String[5];
+	}
+	
+	public String mySchedule(){
+		String finalString = "";
+		if(scheduledMeetings[0].isEmpty()!=true){
+			finalString = "";
+		}
+		
+		
+		return finalString;
+	}
+	
+	
 }
