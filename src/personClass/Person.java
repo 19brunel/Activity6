bd567174126262e80d71f10731d1bc035c651745
Matthew Chiang <mchiang2112@gmail.com>
2018-02-28 16:09:57 -0600

    person v1.1

--- src/personClass/Person.java
+++ src/personClass/Person.java
@@ -1,5 +1,34 @@
 package personClass;
 
 public class Person {
-
+	private String name;
+	private double salary;
+	private Schedule schedule;
+	private Address address;
+	private InterestingFacts interestingFacts;
+	private Projects project;
+	private Occupation occupation;
+	public Person(){
+		name = "Grant Parfrey";
+		salary = 999.9;
+		schedule = new Schedule();
+		address = new Address();
+		interestingFacts = new InterestingFacts();
+		project = new Projects();
+	}
+	public String getAddress(){
+		return address.mailingLabel();
+	}
+	public String getSchedule(){
+		return schedule.mySchedule();
+	}
+	public String getInterestingFacts(){
+		return interestingFacts.aboutMe();
+	}
+	public String getProjects(){
+		return project.projectSummary();
+	}
+	public String getOccupation(){
+		return occupation.nameTag();
+	}
 }
