6b53229eb2a672b0405ef7e34e99ca7b27883b29
Matthew Chiang <mchiang2112@gmail.com>
2018-02-28 15:28:29 -0600

    Projects v1.1

--- src/personClass/Projects.java
+++ src/personClass/Projects.java
@@ -1,9 +1,20 @@
 package personClass;
-import java.util.Scanner;
-import java.io.*;
+import java.util.ArrayList;
 public class Projects {
-	public static void main(String[]args) throws IOException{
+	ArrayList<String> completedProjects = new ArrayList<String>();
+	public Projects(){
 		
+	}
+	public void addProject(String project){
+		completedProjects.add(project);
+	}
+	public String projectSummary(){
+		String summary = null;
+		for (int i = 0; i < completedProjects.size(); i ++){
+			summary = summary + (i+1) + ". " + completedProjects.get(i) + " ";
+		}
+		String summaryEdit = summary.substring(4);
+		return summaryEdit;
 	}
 
 }
